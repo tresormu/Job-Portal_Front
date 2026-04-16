1e19516e8b7d6bc720f0be9257d98ae0e101430e
tresor <tresormugisha07@gmail.com>
2026-04-16 10:45:14 +0200

    bugfix: integration error fix

--- src/services/ApiSetter.ts
+++ src/services/ApiSetter.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.DEV ? "/api" : import.meta.env.VITE_BACKEND_URL_HOST,
+  baseURL:import.meta.env.VITE_BACKEND_URL_HOST,
   timeout: 20000,
   headers: {
     "Content-Type": "application/json",
